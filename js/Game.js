445a5a20bfbc5948181cac1ac3a32e5fecde9170
sahajSinghal <sahajsinghal2007@gmail.com>
2020-08-19 17:53:10 +0530

    hello

--- js/Game.js
+++ js/Game.js
@@ -34,6 +34,11 @@ class Game {
     car3 = createSprite(500,200);
     car4 = createSprite(700,200);
     cars = [car1, car2, car3, car4];
+
+    car1.addImage("car1",car1Img);
+    car2.addImage("car2",car2Img);
+    car3.addImage("car3",car1Img);
+    car4.addImage("car4",car1Img);
   }
 
   play(){
@@ -43,12 +48,14 @@ class Game {
     
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+      background("#c68767");
+
+      image(track,0,-displayHeight * 4,displayWidth,displayHeight * 5);
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -72,6 +79,7 @@ class Game {
         //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
       }
 
+      
     }
 
     if(keyIsDown(UP_ARROW) && player.index !== null){
@@ -79,6 +87,16 @@ class Game {
       player.update();
     }
 
+    if(player.distance > 3860){
+      gameState = 2;
+    }
+
     drawSprites();
+  }
+
+  end()
+  {
+    console.log("gameEnded");
+    game.update(2);
   }
 }
--- sketch.js
+++ sketch.js
@@ -9,7 +9,17 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var car1Img, car2Img, car3Img, car4Img, track, ground;
 
+function preload()
+{
+  car1Img = loadImage("images/car1.png");
+  car2Img = loadImage("images/car2.png");
+  car3Img = loadImage("images/car3.png");
+  car4Img = loadImage("images/car4.png");
+  track = loadImage("images/track.jpg");
+  ground = loadImage("images/ground.png");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -19,13 +29,18 @@ function setup(){
   game.start();
 }
 
-
 function draw(){
   if(playerCount === 4){
     game.update(1);
   }
+
   if(gameState === 1){
     clear();
     game.play();
+  }
+
+  if(gameState === 2)
+  {
+    game.end();
   }
 }
